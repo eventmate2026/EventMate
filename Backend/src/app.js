c226aab0d975c0b57f87219879414573d6fb80f6
eventmate2026 <eventmete@gmail.com>
2026-03-19 12:22:28 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -26,7 +26,7 @@ const corsOriginValidator = createCorsOriginValidator();
 
 // Middleware
 app.use(helmet());
-app.use(cors({ origin: corsOriginValidator(), credentials: true }));
+app.use(cors({ origin: corsOriginValidator, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
